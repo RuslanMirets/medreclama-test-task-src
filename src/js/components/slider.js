094471f71cb99338a7f888c38d49cd6f4977301a
Ruslan <ruslan.mirets@gmail.com>
2022-11-06 15:51:36 +0300

    Mobile Info

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -3,12 +3,35 @@ import Swiper, { Navigation } from "swiper";
 Swiper.use([Navigation]);
 
 const swiper = new Swiper(".specialists__content", {
+  grabCursor: true,
   slidesPerView: 2,
   spaceBetween: 10,
-  grabCursor: true,
 
   navigation: {
     nextEl: ".specialists__slider-btn--next",
     prevEl: ".specialists__slider-btn--prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
   },
 });
